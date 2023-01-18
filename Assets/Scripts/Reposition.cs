9825d1150d6fce710d696a65f73ea65777f63d06
Edoc <79559496+poik1118@users.noreply.github.com>
2023-01-19 02:05:11 +0900

    Update

---
+++ Assets/Scripts/Reposition.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
