13d1d90cab842f63652321737ba4685ac4b111e9
Edoc <79559496+poik1118@users.noreply.github.com>
2023-01-18 21:06:14 +0900

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public Vector2      inputVector;
+    public float        speed;
 
     Rigidbody2D         rigid;
+    SpriteRenderer      spriteRenderer;
 
 
     void  Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
-    {
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
+    void Update(){
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-        rigid.MovePosition(rigid.position + inputVector);
+        Vector2 nextVector = ( inputVector * speed * Time.fixedDeltaTime );
+        rigid.MovePosition( rigid.position + nextVector );
+    }
+
+    void LateUpdate()
+    {
+        if(inputVector.x != 0){
+            spriteRenderer.flipX = inputVector.x < 0;
+        }
     }
 }
